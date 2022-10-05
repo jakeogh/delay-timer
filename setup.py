4bfbc4805e962a00b8a6caf2af17cb11a580291b
jakeogh <github.com@v6y.net>
2022-10-04 20:45:33 -0700

    auto-commit

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ from setuptools import setup
 import fastentrypoints
 
 if not sys.version_info[0] == 3:
-    sys.exit("Python 3 is required. Use: \'python3 setup.py install\'")
+    sys.exit("Python 3 is required. Use: 'python3 setup.py install'")
 
-dependencies = ["icecream", "click", "asserttool"]
+dependencies = []
 
 config = {
     "version": "0.1",
@@ -21,7 +21,7 @@ config = {
     "author_email": "github.com@v6y.net",
     "description": "Short explination of what it does _here_",
     "long_description": __doc__,
-    "packages": find_packages(exclude=['tests']),
+    "packages": find_packages(exclude=["tests"]),
     "include_package_data": True,
     "zip_safe": False,
     "platforms": "any",
