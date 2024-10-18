b97ecf5e4bcfee9e0eafa4b95ea219c7f3787f21
jakeogh <github.com@v6y.net>
2024-10-17 23:44:49 -0700

    auto-commit

--- delay_timer/__init__.py
+++ delay_timer/__init__.py
@@ -1,2 +1,6 @@
-from .delay_timer import DelayTimer
-from .delay_timer import random_delay_seconds
+"""
+isort:skip_file
+"""
+
+from .delay_timer import DelayTimer as DelayTimer
+from .delay_timer import random_delay_seconds as random_delay_seconds
