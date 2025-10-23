6f6cd2541a4a19d0424048e7b5f20eb5b4aa5c54
jakeogh <github.com@v6y.net>
2025-10-22 20:37:43 -0700

    auto-commit

--- delay_timer/delay_timer.py
+++ delay_timer/delay_timer.py
@@ -11,26 +11,37 @@ from random import randrange
 from asserttool import icp
 
 
-def random_delay_seconds(_min: int, _max: int):
-    _delay = randrange(_min, _max + 1)
-    time.sleep(_delay)
+def random_delay_seconds(min_seconds: int, max_seconds: int):
+    """Sleep for a random number of seconds between min_seconds and max_seconds (inclusive)."""
+    delay = randrange(min_seconds, max_seconds + 1)
+    time.sleep(delay)
 
 
 class DelayTimer:
+    """
+    Exponential backoff timer that increases delay between operations.
+
+    Starts at 'start' seconds and multiplies by 'multiplier' after each sleep.
+    If 'end' is specified, delay will not exceed that maximum.
+    If 'end' is None, delay will grow indefinitely.
+    """
+
     def __init__(
         self,
         start: float,
-        end: float,
+        end: float | None = None,
         multiplier: float = 1.5,
     ):
         start = float(start)
         multiplier = float(multiplier)
-        end = float(end)
-        assert start > 0
-        assert end > start
-        # assert multiplier > 0
-        delay = start
-        self.delay = delay
+        if end is not None:
+            end = float(end)
+            assert end > start, "end must be greater than start"
+        assert start > 0, "start must be positive"
+        assert (
+            multiplier > 1.0
+        ), "multiplier must be greater than 1.0 for exponential growth"
+        self.delay = start
         self.multiplier = multiplier
         self.end = end
 
@@ -39,9 +50,14 @@ class DelayTimer:
         time.sleep(self.delay)
 
     def _sleep_next(self):
-        if self.delay < self.end:
-            self.delay = min(self.delay + (self.delay * self.multiplier), self.end)
+        if self.end is None:
+            # No maximum, grow indefinitely
+            self.delay = self.delay * self.multiplier
+        elif self.delay < self.end:
+            # Cap at maximum
+            self.delay = min(self.delay * self.multiplier, self.end)
 
     def sleep(self):
+        """Sleep for the current delay duration, then increase the delay for next time."""
         self._sleep()
         self._sleep_next()
